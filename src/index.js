108ea159d3aa8d464f89ef7fe47fac9619c04f09
Anton Chyhariev <tonichiga@gmail.com>
2021-01-11 19:30:58 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "../node_modules/basiclightbox/dist/basicLightbox.min.css";
 import "@pnotify/core/dist/PNotify.css";
 // import "@pnotify/core/dist/BrightTheme.css";
 import "@pnotify/core/dist/Material.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // import "@pnotify/core/dist/Material.css";
 // import "material-design-icons/iconfont/material-icons.css";
